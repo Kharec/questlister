4471e21da50f9eb11e4496b21072664d98f19c40
Sandro CAZZANIGA <sandro@cazzaniga.fr>
2024-03-03 23:46:43 +0100

    add Run() logic

---
+++ internal/questlister/questlister.go
@@ -0,0 +1,37 @@
+package questlister
+
+import (
+	"github.com/Kharec/questlister/internal/db"
+)
+
+func Run(args []string) {
+
+	database := db.OpenDatabase("database/questlister.db")
+	db.Migrate(database)
+	ql := QL(database)
+
+	if len(args) < 1 {
+		DisplayHelp()
+		return
+	}
+
+	switch args[0] {
+	case "add":
+		ql.Create(args[1])
+
+	case "achieve":
+		ql.MarkAsDone(args[1])
+
+	case "list":
+		PrintQuests(ql.GetAll())
+
+	case "clean":
+		ql.CleanQuests()
+
+	case "help":
+		DisplayHelp()
+
+	case "default":
+		return
+	}
+}
