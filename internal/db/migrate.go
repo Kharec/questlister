2764d90d0fbe5118107d96d57d498479c0ca480c
Sandro CAZZANIGA <sandro@cazzaniga.fr>
2024-03-05 11:55:32 +0100

    handle error in database migration

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -3,8 +3,12 @@ package db
 import (
 	"github.com/Kharec/questlister/internal/models"
 	"gorm.io/gorm"
+	"log"
 )
 
 func Migrate(database *gorm.DB) {
-	database.AutoMigrate(&models.Quest{})
+	err := database.AutoMigrate(&models.Quest{})
+	if err != nil {
+		log.Fatal("database migration failed")
+	}
 }
